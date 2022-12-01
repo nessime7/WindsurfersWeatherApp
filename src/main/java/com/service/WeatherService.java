35cb3907650c07bd8f568d509883ceb1770ae26e
nessime7 <sara.przebinda@gmail.com>
2022-12-01 19:49:46 +0100

    1

--- src/main/java/com/service/WeatherService.java
+++ src/main/java/com/service/WeatherService.java
@@ -32,23 +32,39 @@ public class WeatherService {
 
     // metoda getBestLocation typu WeatherDataResponse z parametrami requestedDate typu LocalDate
     public WeatherDataResponse getBestLocation(LocalDate requestedDate) {
+        // zmienna cities i przypisanie wyniku metody findAll na polu obiektu cityRepository
         final var cities = cityRepository.findAll();
+        // zmienna weathers i zwrócenie wyniku łancucha wywołań na zmiennych cities
         final var weathers = cities.stream()
+                // dla każdego miasta pobiera pogodę z 16 dni
                 .map(restTemplateConfig::getWeather)
+                // przypisanie pobranych danych do listy
                 .collect(Collectors.toList());
+        // zwrócenie wyniku metody resolveWeather o paramatrach requestedDate oraz Listy Weathers
         return resolveWeather(requestedDate, weathers);
     }
 
+    // metody resolveWeather, zwraca typ WeatherDataResponse o parametrach requestedDate typu LocalDate
+    // oraz weathers typu Lista CityData
     private WeatherDataResponse resolveWeather(LocalDate requestedDate, List<CityData> weathers) {
+        // przypisanie do zmiennej typu WeatherDataResponse null
         WeatherDataResponse weatherResult = null;
+        // zdefiniowanie zmiennej lokalne bestLocationValue i przypisanie do niej wartości 0
         final var bestLocationValue = 0;
+        // dla każdego elementu tablicy Weathers przypisujemy zmienną weather
         for (final var weather : weathers) {
+            // zmienna weatherForRequestedDate, przypisanie do niej wyniku metody weatherForRequestedDate
+            // o parametrach requestedDate oraz weather
             final var weatherForRequestedDate = weatherForRequestedDate(requestedDate, weather);
             if ((weatherForRequestedDate.getTemperature() > 5 && weatherForRequestedDate.getTemperature() < 35) &&
                     (weatherForRequestedDate.getWindSpeed() > 5 && weatherForRequestedDate.getWindSpeed() < 18)) {
+                // przypisanie do zmiennej currentCityLocationValue wymiku metody bestLocationCalculator z parametrami
+                // getWindSpeed, getTemperature
                 final var currentCityLocationValue = bestLocationCalculator(weatherForRequestedDate.getWindSpeed(),
                         weatherForRequestedDate.getTemperature());
+                // jeśli wynik zmiennej currentCityLocationValue jest większe niż 0
                 if (currentCityLocationValue > bestLocationValue) {
+                    // zmienna weatherResult przypisanie nowej odpowiedzi WeatherDataResponse o parametach
                     weatherResult = new WeatherDataResponse(weather.getCityName(),
                             weatherForRequestedDate.getWindSpeed(),
                             weatherForRequestedDate.getTemperature());
@@ -62,9 +78,13 @@ public class WeatherService {
     }
 
     private WeatherData weatherForRequestedDate(LocalDate requestedDate, CityData weather) {
+    // zwrócenie wyniku łancucha wywołań na pobranych danych na zmiennej weather typu CityData
         return weather.getData().stream()
+                // filtrujemy i sprawdzamy czy getValidateDate jest równe requestedDate
                 .filter(w -> w.getValidDate().equals(requestedDate))
+                // znajdź pierwszy
                 .findAny()
+                // lub wyrzuć wyjątek
                 .orElseThrow();
     }
 
