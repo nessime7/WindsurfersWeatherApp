749908720d5706c92e50ebe86a0d3d2ee5d9357c
nessime7 <sara.przebinda@gmail.com>
2022-12-02 20:08:18 +0100

    1

--- src/test/java/com/service/WeatherServiceTest.java
+++ src/test/java/com/service/WeatherServiceTest.java
@@ -13,17 +13,19 @@ import java.time.LocalDate;
 import java.util.Set;
 import java.util.UUID;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class WeatherServiceTest {
 
-    @Autowired
-    private WeatherBitApiConnector weatherBitApiConnector;
-
+    private final WeatherBitApiConnector weatherBitApiConnector = mock(WeatherBitApiConnector.class);
     private final CityRepository cityRepository = mock(CityRepository.class);
     private final WeatherService weatherService = new WeatherService(weatherBitApiConnector, cityRepository);
 
@@ -59,4 +61,44 @@ public class WeatherServiceTest {
 //        // then
 //        assertEquals("Warszawa", result.getCityName());
 //    }
+
+    @Test
+    void should_return_better_location_Warsaw() {
+        // given
+        final var firstCityName = new City(UUID.randomUUID(), "Kraków");
+        final var secondCityName = new City(UUID.randomUUID(), "Warszawa");
+        when(cityRepository.findAll()).thenReturn(java.util.List.of(firstCityName, secondCityName));
+        final var firstData = new WeatherData(20.0, 10.0, LocalDate.from(LocalDate.now()));
+        final var secondData = new WeatherData(21.0, 11.0, LocalDate.from(LocalDate.now()));
+        final var firstCity = new CityData("Kraków", "1", "1", Set.of(firstData));
+        final var secondCity = new CityData("Warszawa", "1", "1", Set.of(secondData));
+        when(weatherBitApiConnector.getWeather(firstCityName)).thenReturn(firstCity);
+        when(weatherBitApiConnector.getWeather(secondCityName)).thenReturn(secondCity);
+
+        // when
+        final var result = weatherService.getBestLocation(LocalDate.from(LocalDate.now()));
+
+        // then
+        assertEquals("Warszawa", result.getCityName());
+    }
+
+// nie mockuję wywołań prywatnej klas w tej samej klasie
+
+    @Test
+    void should_return_exception_null_data_response() {
+        // given
+        final var firstCityName = new City(UUID.randomUUID(), "Kraków");
+        final var secondCityName = new City(UUID.randomUUID(), "Warszawa");
+        when(cityRepository.findAll()).thenReturn(java.util.List.of(firstCityName, secondCityName));
+        final var firstData = new WeatherData(2.0, 1.0, LocalDate.from(LocalDate.now()));
+        final var secondData = new WeatherData(2.0, 1.0, LocalDate.from(LocalDate.now()));
+        final var firstCity = new CityData("Kraków", "1", "1", Set.of(firstData));
+        final var secondCity = new CityData("Warszawa", "1", "1", Set.of(secondData));
+        when(weatherBitApiConnector.getWeather(firstCityName)).thenReturn(firstCity);
+        when(weatherBitApiConnector.getWeather(secondCityName)).thenReturn(secondCity);
+
+        // then
+        var ex = assertThrows(IllegalStateException.class, () -> weatherService.getBestLocation(LocalDate.from(LocalDate.now())));
+        assertThat(ex.getMessage(), is("No matching city found."));
+    }
 }
