04ca8da4db563e38919c7defe71c0ccca17cd28e
nessime7 <sara.przebinda@gmail.com>
2022-12-07 01:02:52 +0100

    1

--- src/test/java/com/integration/WeatherRestAssuredAndWireMockIntegrationTest.java
+++ src/test/java/com/integration/WeatherRestAssuredAndWireMockIntegrationTest.java
@@ -1,59 +1,57 @@
 package com.integration;
 
-import com.WindsurfersWeatherAppSaraApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.rest.WeatherDataResponse;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@ExtendWith(SpringExtension.class)
-// @SpringBootTest(classes = {WindsurfersWeatherAppSaraApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 @SpringBootTest
 @WireMockTest(httpsEnabled = true, httpPort = 8081)
 @AutoConfigureMockMvc
 public class WeatherRestAssuredAndWireMockIntegrationTest {
 
-//    @LocalServerPort
-//    private int port;
-//    private static final String CONTEXT = "weather";
-
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void should_check_weather_and_return_200() throws Exception {
-        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=Jastarnia&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
-                .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                .withBody("response/jastarnia-response.json")));
-        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=Bridgetown&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
-                .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                .withBody("response/bridgetown-response.json")));
-        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=Fortaleza&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
-                .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                .withBody("response/fortaleza-response.json")));
-        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=Pissouri&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
-                .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                .withBody("response/response/pissouri-response.json")));
-        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=LeMorne&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
-                .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                .withBody("response/response/leMorne-response")));
+        // given
+        stubFor(get(urlEqualTo("/v2.0/forecast/daily"))
+                .withQueryParam("city", equalTo("Jastarnia"))
+                .withQueryParam("key", equalTo("c84ad94814fb4457b070f396b4029306"))
+                .willReturn(aResponse()
+                        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("response/jastarnia-response.json")));
 
-        this.mockMvc.perform(
-                        MockMvcRequestBuilders.get("/weather/2022-12-05")
-                                .accept("application/json"))
+        // when
+        var mvcResult = mockMvc.perform(
+                        MockMvcRequestBuilders.get("/weather/2022-12-06"))
+                .andDo(print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().json("{}"));
+                .andReturn();
+
+        // then
+        var response = objectMapper.readValue(mvcResult.getResponse()
+                .getContentAsString(UTF_8), WeatherDataResponse.class);
+
+        assertEquals("Jastarnia", response.getCityName());
+        verify(getRequestedFor(urlPathEqualTo("/v2.0/forecast/daily")));
     }
 }
 
@@ -65,3 +63,28 @@ public class WeatherRestAssuredAndWireMockIntegrationTest {
 //                .and()
 //                .body("", equalTo(TestUtils.getPath("response/weather-response.json", CONTEXT).get("")));
 //         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+//    @LocalServerPort
+//    private int port;
+//    private static final String CONTEXT = "weather";
+
+///////////////////////////////////////
+
+// @ExtendWith(SpringExtension.class)
+// @SpringBootTest(classes = {WindsurfersWeatherAppSaraApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+//    stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=Jastarnia&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
+//        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
+//        .withBody("response/jastarnia-response.json")));
+//        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=Bridgetown&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
+//        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
+//        .withBody("response/bridgetown-response.json")));
+//        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=Fortaleza&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
+//        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
+//        .withBody("response/fortaleza-response.json")));
+//        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=Pissouri&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
+//        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
+//        .withBody("response/response/pissouri-response.json")));
+//        stubFor(get(urlEqualTo("/v2.0/forecast/daily?city=LeMorne&key=c84ad94814fb4457b070f396b4029306")).willReturn(aResponse()
+//        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
+//        .withBody("response/response/leMorne-response")));
